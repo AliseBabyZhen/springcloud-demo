ac0eb8e28ef26f4231af839affd5cd9faf522f83
代码白痴 <babyzhen1206@163.com>
2021-08-23 16:42:46 +0800

    增加了删除服务并且支持调用的

--- order-server/src/main/java/com/yf/feign/ProductFeginClient.java
+++ order-server/src/main/java/com/yf/feign/ProductFeginClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 再拼接上@RequestMapping(value = "/product/{id}",method = RequestMethod.GET)
  * 这里相当于一个自定的API 调用这个方法就会发起这个对product-server的http请求
  * productserver那边controller收到请求进行处理然后返回数据给到这个边
- * 在调用那边的删除服务时 忘了要注意返回类型，那边返回了一个OK 方法要定义成String
+ * 在调用那边的删除服务时 忘了要注意返回类型，那边返回了一个OK 方法要定义成String 。。
  * @USER:
  * @DATE: 2021/8/22 下午9:16
  */
